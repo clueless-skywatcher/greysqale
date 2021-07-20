9a9e1da6a3972103cc6ff9a0f83a90a9f2371671
clueless-skywatcher <somichat@gmail.com>
2021-07-20 21:50:12 +0530

    Added .env support for examples.py

--- examples.py
+++ examples.py
@@ -4,10 +4,12 @@ from greysqale.fields import IntegerField, IDField, VarcharField
 from greysqale.query import CreateTable
 from greysqale.table import ModelTable
 
+from decouple import config
+
 class Employee(ModelTable):
     name = VarcharField(name = 'name', constraints = [NotNull()])
     age = IntegerField(name = 'age', constraints = [NotNull()])
     salary = IntegerField(name = 'salary', constraints = [NotNull()])
 
-db = GSQLDatabase('postgres', 'postgres', 'testdb')
+db = GSQLDatabase(config('POSTGRES_USERNAME'), config('POSTGRES_PASSWORD'), config('POSTGRES_DBNAME'))
 db.add(Employee)
